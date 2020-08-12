f8ca3da834a68c41f03a6ccd022f5e4cf1c442c1
PhamLeQuangNhat <18520120@gm.uit.edu.vn>
2020-08-13 06:40:38 +0700

    Update web

---
+++ Final/web/application.py
@@ -0,0 +1,30 @@
+import os, time
+from flask import Flask, render_template, request, jsonify
+import fruit_pred
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["DEBUG"] = 0
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        data = request.form.get("img-data")
+        data = data.split(',')
+        data = [int(num) for num in data]
+        with open("static/image/new_image","wb") as f:
+            f.write(bytearray(data))
+        response = fruit_pred.predict()
+        return render_template("index.html", response=response)
+    return render_template("index.html")
+
+
+@app.route("/prediction", methods=["GET","POST"])
+def prediction():
+    data = request.form.get("img-data")
+    data = data.split(',')
+    data = [int(num) for num in data]
+    with open("static/image/new_image","wb") as f:
+        f.write(bytearray(data))
+    result = fruit_pred.predict() + 1
+    return render_template("predict.html", prediction=result)
---
+++ Final/web/fruit_pred.py
@@ -0,0 +1,59 @@
+import cv2
+import imutils
+import joblib
+from skimage import feature
+def preprocess(image, width, height):
+     
+        # grab the dimesions of the image and then initialize
+        # the deltas to use when cropping
+        (h, w) = image.shape[:2]
+        dW = 0
+        dH = 0
+
+        # if the width is smller than the height,so resize
+        # along the width and then update the deltas 
+        # to crop the height to the desired demension
+        if w < h:
+            image =imutils.resize(image, width=width, inter=cv2.INTER_AREA)
+            dH = int((image.shape[0] - height) / 2.0)
+
+        # otherwise, the height is smaller than the width so 
+        # resize along the height and then update the deltas 
+        # to crop along the width
+        else:
+            image = imutils.resize(image, height=height, inter=cv2.INTER_AREA)
+            dW = int((image.shape[1] - width) / 2.0)
+
+        # now that our images have been resized, we need to 
+        # re-grab the width and height, followed by performing
+        # the crop
+        (h,w) = image.shape[:2]
+        image = image[dH:h -dH, dW:w-dW]
+
+        # finally, resize the image to the provided spatial
+        # demensions to ensure ourput image is slways a fixed
+        # size
+        image = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        hg = feature.hog(gray, orientations=9, pixels_per_cell=(10, 10),
+				        cells_per_block=(2, 2), transform_sqrt=True, block_norm="L1")
+        return hg
+
+def predict():
+    class_name = ['Apple','Avocado','Banana','Coconut','Custard_apple',
+                'Dragon_fruit','Guava','Mango','Orange','Plum',
+                'Start_fruit','Watermelon']
+    model_path = 'model/svm_hog.sav'
+    image_path = 'static/image/new_image'
+    model = joblib.load(model_path)
+
+    image = cv2.imread(image_path)
+    image = preprocess(image,32,32)
+    image = image.astype("float") 
+    image = image.reshape((1,144))
+    preds = model.predict(image)[0]
+    #print(preds)
+    #print(image.shape) 
+    # return class_name[preds]
+    return preds
+
